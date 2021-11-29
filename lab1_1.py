fbc495198663bb285087f9d3b64ae243c2ff0b00
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-11-29 11:00:26 -0500

    add lab1_1.py and lab1_2.py

---
+++ lab1_1.py
@@ -0,0 +1,9 @@
+# Author: ATN 11/29/21
+
+def greeting():
+    '''This function can only print "Hello World" on one line.'''
+    print("Hello World!")
+
+
+help(greeting)
+greeting()
---
+++ lab1_2.py
@@ -0,0 +1,9 @@
+# Author: ATN 11/29/21
+
+def find_sum(num1, num2):
+    new_sum = num1 + num2
+    return new_sum
+
+
+my_sum = find_sum(111, 222)
+print(my_sum)
